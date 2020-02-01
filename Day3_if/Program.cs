9b768c90ae6782fef334c54e7a5b24970075a8a0
Maksims Tabaks <maksims.tabaks@visma.com>
2020-02-01 13:56:32 +0200

    day3 methods

--- Day3_if/Program.cs
+++ Day3_if/Program.cs
@@ -6,7 +6,111 @@ namespace Day3_if
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("insert first digit");
+			string input = Console.ReadLine();
+			int a = Convert.ToInt32(input);
+
+			Console.WriteLine("insert 2nd digit");
+			input = Console.ReadLine();
+			int b = Convert.ToInt32(input);
+
+			Console.WriteLine("insert 3rd digit");
+			input = Console.ReadLine();
+			int c = Convert.ToInt32(input);
+
+			if (a > b && a > c)
+			{
+				Console.WriteLine("1st was the largest" + a);
+			}
+			else if (b > a && b > c)
+			{
+				Console.WriteLine("2nd was the largest" + b);
+			}
+			else if (c > a && c > b)
+			{
+				Console.WriteLine("3rd was the largest" + c);
+			}
+			else if (a == b || b == c || c == b)
+			{
+				Console.WriteLine("2 of them were equal" + a);
+			}
+			else
+			{
+				Console.WriteLine("all are equal");
+			}
+
+
+
+		}
+
+		static void NestingIf()
+		{
+			int a = 5;
+			int b = 6;
+
+			// && AND
+			// || OR
+
+			if (a >= 5 && b == 6)
+			{
+
+			}
+
+			int digit = 5;
+
+			if (digit > 5)
+			{
+				Console.WriteLine("#");
+			}
+			else if (digit < 5)
+			{
+				Console.WriteLine("##");
+			}
+			else
+			{
+				Console.WriteLine("###");
+			}
+
+		}
+
+		static void Calc()
+		{
+			Console.WriteLine("Insert first digit");
+			string input = Console.ReadLine();
+			int a = Convert.ToInt32(input);
+
+
+			Console.WriteLine("+ or - or * or /");
+			string math = Console.ReadLine();
+
+			Console.WriteLine("insert 2nd digit");
+			input = Console.ReadLine();
+			int b = Convert.ToInt32(input);
+
+			double result = 0;
+
+			if (math == "+")
+			{
+				result = a + b;
+			}
+			else if (math == "-")
+			{
+				result = a - b;
+			}
+			else if (math == "*")
+			{
+				result = a * b;
+			}
+			else if (math == "/")
+			{
+				result = a / b;
+			}
+			else
+			{
+				Console.WriteLine("incorrect mathematical action action");
+			}
+
+			Console.WriteLine("Result: " + result);
 		}
 	}
 }
---
+++ Day3_methods/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Day3_methods
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("#1 Insert 2 digits on separate lines:");
+			string input = Console.ReadLine();
+			int a = Convert.ToInt32(input);
+			input = Console.ReadLine();
+			int b = Convert.ToInt32(input);
+
+			Method1(a, b);
+
+			Console.WriteLine("#2Result: " + Method2());
+
+			Console.WriteLine("#3Result: " + Method3(a, b));
+
+		}
+
+		static void Method1(int a, int b)
+		{
+			int result = a + b;
+			Console.WriteLine("#1Result: " + result);
+		}
+
+		static int Method2()
+		{
+			Console.WriteLine("#2 Insert 2 digits on separate lines:");
+			string input = Console.ReadLine();
+			int a = Convert.ToInt32(input);
+			input = Console.ReadLine();
+			int b = Convert.ToInt32(input);
+
+			return a + b;
+		}
+
+		static int Method3(int a, int b)
+		{
+			Console.WriteLine("Reusing var from #1");
+			int result = a + b;
+			return result;
+		}
+
+
+
+
+		static void Ex1()
+		{
+			Console.WriteLine("void method output");
+			Ex2();
+		}
+		static void Ex2()
+		{
+			Console.WriteLine("2nd void method output");
+		}
+
+		static void Ex3(int a, int b)
+		{
+			Console.WriteLine(a + b);
+		}
+
+		static int Ex4()
+		{
+			int a = 5;
+			int b = 6;
+
+			return a + b;
+		}
+	}
+}
