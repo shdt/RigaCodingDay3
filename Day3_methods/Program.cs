869a20d305a2422426f990966fafbe0ce7a43e45
Maksims Tabaks <maksims.tabaks@visma.com>
2020-02-10 16:20:51 +0200

    commit

--- Day3_methods/Program.cs
+++ Day3_methods/Program.cs
@@ -4,7 +4,83 @@ namespace Day3_methods
 {
 	class Program
 	{
+		static int seq = 1;
 		static void Main(string[] args)
+		{
+
+			Random r = new Random();
+			int a = r.Next(1, 7);
+			Console.WriteLine(a);
+			Console.WriteLine(Enum.GetName(typeof(DayOfWeek),a));
+			Console.ReadLine();
+
+		}
+
+		static void InputTrueFalse()
+		{
+			int input1 = AskInput();
+			Console.WriteLine(MethodTrueFalse(input1));
+		}
+
+			static bool MethodTrueFalse(int a)
+		{
+			bool b = true;
+			if (a > 0)
+			{
+				return b;
+			}
+			else
+			{
+				return !b;
+			}
+
+		}
+
+		static void RefactorBiggest()
+		{
+			//Cilveks ievada tris skaitlus- int
+			//Jaizvada lielakais
+
+			int skaitlis1 = AskInput();
+			int skaitlis2 = AskInput();
+			int skaitlis3 = AskInput();
+
+			int biggest = 0;
+			biggest = BiggestofTwo(skaitlis1, skaitlis2);
+			biggest = BiggestofTwo(skaitlis3, biggest);
+
+			Console.WriteLine("Rezultats ir " + biggest);
+
+		}
+
+		static int AskInput()
+		{
+			Console.WriteLine($"Ievadiet skaitli #{seq}!");
+			int result = InputToDigit();
+			seq++;
+			return result;
+		}
+
+		static int InputToDigit()
+		{
+			return Convert.ToInt32(Console.ReadLine());
+		}
+
+		static int BiggestofTwo(int a, int b)
+		{
+			if (a > b)
+			{
+				return a;
+			}
+			else
+			{
+				return b;
+			}
+		}
+
+
+
+		static void Methods3to1()
 		{
 			Console.WriteLine("#1 Insert 2 digits on separate lines:");
 			string input = Console.ReadLine();
@@ -19,6 +95,7 @@ namespace Day3_methods
 			Console.WriteLine("#3Result: " + Method3(a, b));
 
 		}
+
 
 		static void Method1(int a, int b)
 		{
